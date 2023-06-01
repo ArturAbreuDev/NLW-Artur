060fd7873f7245886aa8c774ae8dcf83337c7cb9
Artur Damaceno Abreu <115594906+ArturAbreuDev@users.noreply.github.com>
2023-06-01 10:35:38 -0300

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://nlw-artur-backend.vercel.app/',
+  baseURL: 'https://nlw-artur-backend-ah3spl704-arturabreudev.vercel.app/',
 })
