6a7fe6aa175de8c27997a10914129297a545b942
Artur Damaceno Abreu <115594906+ArturAbreuDev@users.noreply.github.com>
2023-06-01 09:46:52 -0300

    Update NewMemoryForm.tsx

--- src/components/NewMemoryForm.tsx
+++ src/components/NewMemoryForm.tsx
@@ -1,14 +1,13 @@
 'use client'
 
-import { FormEvent, useState } from 'react'
 import { Camera } from 'lucide-react'
-import Cookie from 'js-cookie'
 import { MediaPicker } from './MediaPicker'
+import { FormEvent } from 'react'
 import { api } from '@/lib/api'
+import Cookie from 'js-cookie'
 import { useRouter } from 'next/navigation'
 
 export function NewMemoryForm() {
-  const [loading, setLoading] = useState(false)
   const router = useRouter()
 
   async function handleCreateMemory(event: FormEvent<HTMLFormElement>) {
@@ -43,8 +42,6 @@ export function NewMemoryForm() {
 
     const token = Cookie.get('token')
 
-    setLoading(true)
-
     await api.post(
       '/memories',
       {
@@ -59,15 +56,11 @@ export function NewMemoryForm() {
       },
     )
 
-    router.replace('/?success')
+    router.push('/')
   }
 
   return (
-    <form
-      onSubmit={handleCreateMemory}
-      className="flex flex-1 flex-col gap-2"
-      id="form"
-    >
+    <form onSubmit={handleCreateMemory} className="flex flex-1 flex-col gap-2">
       <div className="flex items-center gap-4">
         <label
           htmlFor="media"
@@ -76,6 +69,7 @@ export function NewMemoryForm() {
           <Camera className="h-4 w-4" />
           Anexar mídia
         </label>
+
         <label
           htmlFor="isPublic"
           className="flex items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100"
@@ -84,25 +78,25 @@ export function NewMemoryForm() {
             type="checkbox"
             name="isPublic"
             id="isPublic"
-            value="false"
+            value="true"
             className="h-4 w-4 rounded border-gray-400 bg-gray-700 text-purple-500"
           />
           Tornar memória pública
         </label>
       </div>
+
       <MediaPicker />
+
       <textarea
         name="content"
         spellCheck={false}
         className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
         placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
       />
+
       <button
         type="submit"
-        disabled={loading}
-        className={`inline-block self-end rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600 ${
-          loading && 'cursor-not-allowed'
-        }`}
+        className="inline-block self-end rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
       >
         Salvar
       </button>
