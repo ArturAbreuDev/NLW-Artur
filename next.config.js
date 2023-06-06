209b053df97fb9db297c7bdf8d941adf48ac2625
arturabreudev <arturabreu110@gmail.com>
2023-06-06 13:19:39 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com', '10.52.4.174'],
+    domains: [
+      'avatars.githubusercontent.com',
+      '10.52.4.191',
+      'github.com',
+      'ik.imagekit.io',
+      'https://nlw-artur-backend.vercel.app/', // Adicione o domínio aqui
+    ],
   },
 }
 
